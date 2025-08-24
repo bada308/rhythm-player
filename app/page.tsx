689838a27002612c8b955d785f77721a66cb5408
bada308 <gangbada890@gmail.com>
2025-08-24 18:20:47 +0900

    feat: 버튼 추가 및 페이지 이동 로직 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-	return <div className=""></div>;
+"use client";
+
+import { Button } from "@heroui/react";
+import { useRouter } from "next/navigation";
+
+export default function Page() {
+	const router = useRouter();
+	return (
+		<div>
+			<Button color="primary" onPress={() => router.push("/create-sheet")}>
+				생성
+			</Button>
+		</div>
+	);
 }
