6b32046b6409b3b6783879af86d1ad37f2a9d116
bada308 <gangbada890@gmail.com>
2025-08-24 16:24:49 +0900

    refactor: PropsWithChildren 타입 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
-export default function RootLayout({
-	children,
-}: Readonly<{
-	children: React.ReactNode;
-}>) {
+import type {PropsWithChildren} from "react";
+
+export default function RootLayout({ children }: Readonly<PropsWithChildren>) {
 	return (
 		<html lang="ko">
 			<body>{children}</body>
